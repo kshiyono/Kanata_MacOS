94299b3eac3dd150fda036fe23bec6bb4ca4cb11
kshiyono <k.shiyo.no@gmail.com>
2019-08-15 11:36:58 +0900

    [add] Kanata - 個人タスク予実管理ツール 新規作成

---
+++ Kanata_MacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Kanata_MacOS
+//
+//  Created by Katsuyama Shota on 2019/08/15.
+//  Copyright © 2019 Katsuyama Shota. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Kanata_MacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Kanata_MacOS
+//
+//  Created by Katsuyama Shota on 2019/08/15.
+//  Copyright © 2019 Katsuyama Shota. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
